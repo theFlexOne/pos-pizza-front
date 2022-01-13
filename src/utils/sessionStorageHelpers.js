9f7bc76f522812b7521c5f9e52aed6755763be40
flexone <81645330+theFlexOne@users.noreply.github.com>
2022-01-12 23:54:06 -0600

    removed unnecessary files

--- src/utils/sessionStorageHelpers.js
+++ src/utils/sessionStorageHelpers.js
@@ -16,3 +16,26 @@ const removeCustomerFromSS = ({ id }) => {
 };
 
 export { addCustomerToSS, getFromSS, removeCustomerFromSS };
+
+/*
+const addToSessionsStorage = data => {
+  for (const section in data) {
+    sessionStorage.setItem(`${section}`, JSON.stringify(data[section]));
+  }
+};
+
+const updateSessionStorage = (key, data) => {
+  sessionStorage.setItem(`${key}`, JSON.stringify(data));
+};
+
+const getFromSessionStorage = arg => {
+  if (Array.isArray(arg)) {
+    const data = arg.map(key => sessionStorage.getItem(`${key}`));
+    return { ...data };
+  }
+  const key = arg;
+  const data = JSON.parse(sessionStorage.getItem(`${key}`));
+  return data;
+};
+
+*/
