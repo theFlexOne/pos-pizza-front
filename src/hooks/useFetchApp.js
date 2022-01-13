169b8fbad568cc105ff8b11eebef36bd4dcc516e
flexone <81645330+theFlexOne@users.noreply.github.com>
2022-01-13 09:29:36 -0600

    cleaned up code

--- src/hooks/useFetchApp.js
+++ src/hooks/useFetchApp.js
@@ -9,14 +9,6 @@ const populateSessionStorage = data => {
   });
 };
 
-// const postNewCustomer = async customer => {
-//   const res = await fetch(POS_DATA_URL + '/customers', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(customer),
-//   });
-// };
-
 const useFetchApp = () => {
   const [results, setResult] = useState(null);
   const [error, setError] = useState(false);
